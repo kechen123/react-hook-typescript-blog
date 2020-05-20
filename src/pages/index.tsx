aa42bef17abd7524e21a2aea0da409333e21b047
kechen <k13772445714@163.com>
2020-05-20 16:12:19 +0800

    首页，导航动画优化

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,31 +1,14 @@
 import React, { useState, useEffect } from 'react'
 import '@less/index.less'
-
 const Index = () => {
-	const [sign, setSign] = useState('')
-	const [k, setK] = useState(0)
-	var signString = '从你叫什么名字开始<br/><br/>后来<br/><br/>有了一切'
-	const typing = () => {
-		setTimeout(() => {
-			const str = signString.slice(k, k + 1)
-			if (str === '<') {
-				setK(k + 10)
-			} else {
-				setK(k + 1)
-			}
-		}, 200)
-	}
-	useEffect(() => {
-		setSign(signString.slice(0, k))
-		if (k < signString.length) typing()
-	}, [k])
 	return (
-		<div className="h_body">
-			<div className="h_content">
-				{/* <div className="h_name ">柯大晨</div> */}
-				{/* <div className="h_sign" dangerouslySetInnerHTML={{ __html: sign }}></div> */}
+		<section>
+			<div className="info">
+				<h1>Hi, my name is</h1>
+				<h2>kechen</h2>
+				<h3>I build things for the web.</h3>
 			</div>
-		</div>
+		</section>
 	)
 }
 export default Index
